e7a19feeeef41ecbd1bc7c6a424e4a1cd45f2f99
Zhaopengcheng <pengcheng57@163.com>
2021-09-26 10:09:29 +0800

    动态添加reducer模块

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,10 +1,18 @@
 import { AnyAction, Reducer } from 'redux'
 import { combineReducers } from 'redux-immutable'
 
-import {reducer as helloReducer} from '@/pages/hello-world/store'
+interface ReducersType {
+  [key: string]: any
+}
 
-const mainReducer:Reducer<any, AnyAction> = combineReducers({
-  hello: helloReducer
+const reducers: ReducersType = {}
+// @ts-ignore
+const files = require.context('@/pages', true, /store\/index\.ts$/)
+files.keys().forEach((key: string) => {
+  const reducerPath = key.replace('./', '')
+  const reducerName = reducerPath.split('/store/')[0]
+  reducers[reducerName] = require(`@/pages/${reducerPath}`).reducer
 })
+const mainReducer: Reducer<any, AnyAction> = combineReducers(reducers)
 
 export default mainReducer
