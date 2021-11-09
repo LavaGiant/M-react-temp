2b918a6907480a7b870545b3642bc8e4ccca637c
Zhaopengcheng <pengcheng57@163.com>
2021-11-09 10:14:07 +0800

    feat:新增自适应函数以及对象全局类型检查

---
+++ src/utils/fit.ts
@@ -0,0 +1,14 @@
+export const onloadLayout = (): void => {
+  //获取当前浏览器窗口宽度(这里的实质就是body宽度)
+  let win_w: number = document.body.offsetWidth;
+  //定义变量
+  let fontSize: number = 16;
+  //如果浏览器窗口宽度(这里的实质就是body宽度)值小于320，取320
+  win_w = Math.max(320, win_w);
+  fontSize = win_w / 1600 * 16
+  //设置根元素的大小
+  document.getElementsByTagName('html')[0].style.fontSize = fontSize + 'px';
+  //浏览器窗口宽度发生变化时条用这个函数，方便与改变窗口大小时候调试
+  //@ts
+  window.onresize = onloadLayout;
+}
