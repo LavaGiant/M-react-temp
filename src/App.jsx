895e87ee91dd3c623ba95ef849d79b863a00b969
Zhaopengcheng <pengcheng57@163.com>
2021-12-02 20:30:37 +0800

    feat:增加js模板

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import { memo } from 'react'
-import { Provider } from 'react-redux'
 
 import Router from '@/router'
-import store from '@/store'
 
 const App = memo(() => (
-  <Provider store={store}>
     <Router />
-  </Provider>
 ))
 
 export default App
