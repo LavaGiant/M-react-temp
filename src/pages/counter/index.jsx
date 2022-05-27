8ddfc95799208d7d58b92b3d22afb02af2bd8704
Malphite <zhaopengcheng@1228399102447234.onaliyun.com>
2022-05-27 10:11:26 +0800

    chore: 计算工具变动

--- src/pages/counter/index.jsx
+++ src/pages/counter/index.jsx
@@ -19,7 +19,7 @@ const Counter = memo(() => {
       <div className="count">
         {count}
       </div>
-      <InputNumber min={1} max={100} value={inputValue} onChange={setInputValue} />
+      <InputNumber min={0.01} max={100} value={inputValue} onChange={setInputValue} />
       <div className="control">
         <Button type="primary" size="large" shape="circle" onClick={() => dispatch(handleCountAction(inputValue, 'add'))}>+</Button>
         <Button type="primary" size="large" shape="circle" onClick={() => dispatch(handleCountAction(inputValue, 'sub'))}>-</Button>
